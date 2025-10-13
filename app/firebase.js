5116e70121ead40e04f909bc701f37c26f2fdd10
Marcovy <marcovydelacruz22@gmail.com>
2025-10-14 00:28:28 +0800

    delacruz-auth

--- app/firebase.js
+++ app/firebase.js
@@ -1,19 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyBM4iHmEKiVjdSC1sYPfHIU0hQPeNoWXtE",
+  authDomain: "delacruz-auth.firebaseapp.com",
+  projectId: "delacruz-auth",
+  storageBucket: "delacruz-auth.firebasestorage.app",
+  messagingSenderId: "656059684001",
+  appId: "1:656059684001:web:fd8f08e7a5f06d82394ce0",
+  measurementId: "G-Q9P1005CJ1"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const auth = getAuth(app);
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="p-4">
-      <h1>Home Page</h1>
+      <h1>Dela Cruz</h1>
     </main>
   )
 }
